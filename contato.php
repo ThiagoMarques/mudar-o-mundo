822ea23de75664a758fcfe311e285c762eeea2be
thiagomarques <thiagossmarques@gmail.com>
2019-10-16 03:39:39 -0300

    Ajusta tela de contato

--- contato.php
+++ contato.php
@@ -15,34 +15,6 @@
     <link rel="stylesheet" href="assets/css/bootstrap.css" />
     <!--===============================================================================================-->	
     <link rel="stylesheet" href="assets/css/main.css" />
-    <!--====================== Global site tag (gtag.js) - Google Analytics =========================-->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-149525971-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'UA-149525971-1');
-        </script>
-    <!--====================== End Global site tag (gtag.js) - Google Analytics =====================-->
-    <!--=========================== Facebook Pixel Code --><!--==================================-->
-        <script>
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '979073025765599');
-        fbq('track', 'PageView');
-    </script>
-    <noscript><img height="1" width="1" style="display:none"
-    src="https://www.facebook.com/tr?id=979073025765599&ev=PageView&noscript=1(44 B)
-    https://www.facebook.com/tr?id=979073025765599&ev=PageView&noscript=1
-    "
-    /></noscript>
-    <!--====--><!--==================End Facebook Pixel Code --><!--==================================-->
 </head>
 
 <body>
