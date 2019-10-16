4a76a2ddbc01d297b24ac75b607225d933b59af0
Lucas Ataides <lcsataides@gmail.com>
2019-10-15 22:09:10 -0300

    adicionei o arquivo navbar.php, editei o cabeçalho para header.php, incorporei alguns estilos de tipografia

--- includes/header.php
+++ includes/header.php
@@ -5,50 +5,15 @@
 <head>
     <title>Mudar o Mundo</title>
     <meta charset="utf-8">
-    <!--===============================================================================================-->	
+    <!--===============================================================================================-->
     <link rel="icon" href="assets/images/icon.png">
-    <!--===============================================================================================-->	
-    <link rel="stylesheet" href="assets/css/bootstrap.css" />
-    <!--===============================================================================================-->	
-    <link rel="stylesheet" href="assets/css/main.css" />
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/image" href="assets/images/icons/icon.png" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="assets/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    <!--===============================================================================================-->
 </head>
-
-<body>
-    <nav class="navbar navbar-expand-lg">
-        <div class="align-menu">
-            <div id="header" class="mobileNav navbar-toggler">
-                <a class="nav-link" href="index.php">Home</a>
-            </div>
-            <div id="header" class="mobileNav navbar-toggler">
-            <a class="nav-link" href="ingressos.php">Ingressos</a>
-            </div>
-            <div id="header" class="mobileNav navbar-toggler">
-            <a class="nav-link" href="contato.php">Contato</a>
-            </div>
-            <div id="header" class="collapse navbar-collapse">
-                <ul class="navMenu navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="ingressos.php">Ingressos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="contato.php">Contato</a>
-                    </li>
-                </ul>
-            </div>
-            
-        </div>
-        <div>
-            <a href="http://facebook.com/mudaromundobr"
-                        class="fa fa-facebook-square"><span class="label"></span></a></li>
-            <a href="http://instagram.com/mudaromundobrazil"
-                        class="fa fa-instagram"><span class="label"></span></a></li>
-        </div>
-    </nav>
-    <script src="assets/js/bootstrap.min.js"></script>
-
-</body>
-
-</html>
---
+++ includes/navbar.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg">
+  <a class="navbar-brand" href="#"><img src="assets/imgs/logo-mudaromundo.png" alt="#MudaroMundo"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#header" aria-controls="header" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="fa fa-arrow menu-icon"></span>
+  </button>
+  <div id="header" class="collapse navbar-collapse">
+    <ul class="navMenu navbar-nav mx-auto">
+      <li class="nav-item">
+          <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+          <a class="nav-link" href="ingressos.php">Ingressos</a>
+      </li>
+      <li class="nav-item">
+          <a class="nav-link" href="contato.php">Contato</a>
+      </li>
+    </ul>
+  </div>
+
+  <div>
+    <ul class="social-link ml-auto">
+      <a href="http://facebook.com/mudaromundobr"><span class="fa fa-facebook-square fa-2x"></span></a>
+      <a href="http://instagram.com/mudaromundobrazil"><span class="fa fa-instagram fa-2x"></span></a>
+  </div>
+</nav>
